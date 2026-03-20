9c6ccc5df4e15e42784afc83d7bc9dd35d145779
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-20 11:01:29 -0300

    Model

---
+++ src/main/java/com/Carreira/PA_GerenciadorDeCategorias/PaGerenciadorDeCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeCategorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaGerenciadorDeCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaGerenciadorDeCategoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Carreira/PA_GerenciadorDeCategorias/PaGerenciadorDeCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeCategorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaGerenciadorDeCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
